4e52896b98c23a0a9dae9dc8134c1131aaf7fb08
Shibaz98 <shibaz98@gmail.com>
2025-01-08 18:46:02 +0000

    found a way to remove each item, now need to dynamically update the price each time an item is removed

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useState} from "react";
 
 
 export default function Checkout({order}) {
 
+  const initialSummary = order.reduce((accumulator, order) => {
+    const food = order.meal;
+    accumulator[food] = (accumulator[food] || 0) + 1;
+    return accumulator;
+  }, {});  // this returns an object of the food and the total number of food items
+  
+  console.log(initialSummary); 
+  
+  const [summary, setSummary] = useState(initialSummary); // set the initial state of the summary to the initialSummary
   
 
   console.log(order)
+
+  
 
   let totalCost = 0;
 
@@ -19,17 +30,28 @@ export default function Checkout({order}) {
 
   } // total cost of the order using the reduce method, parsloaft changes the string to a number and the replace method removes the £ sign
 
-const summary = order.reduce((accumulator, order) => {
-  const food = order.meal;
-  accumulator[food] = (accumulator[food] || 0) + 1;
-  return accumulator;
-}, {});  // this returns an object of the food and the total number of food items
 
-console.log(summary); 
+const handleRemove = (food) => {
+  setSummary((prevsummary) =>{
+    const updatedSummary = {...prevsummary};
+    if (updatedSummary[food] > 1) {
+      updatedSummary[food] -= 1;
+    } else {
+      delete updatedSummary[food];
+    }
+    return updatedSummary;
+  })
+}
+
 
 
 let orderSummary = Object.entries(summary).map(([food, quantity]) => {
-  return <li key={quantity}>{food} x {quantity}</li>
+  return <li key={food}>
+            {food} x {quantity} 
+            <button onClick={() => handleRemove(food)}>Remove</button> 
+          </li>
+  
+  
 }) // Object.entries returns an array of the object, map through the array and return the food and quantity of the food, [food, quantity] is extracting the key value object and then being returned as a list
 
 
@@ -38,9 +60,7 @@ let orderSummary = Object.entries(summary).map(([food, quantity]) => {
   return (
     <div className="checkout">
         <h1>Order summary</h1>
-        
-          {orderSummary}
-        
+          {orderSummary}  
         <h3>Order total: £{totalCost.toFixed(2)}</h3>
     </div>
   )
